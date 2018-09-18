8fac0fd263c9fd1123162ad2f9083562e22a8002
BartJolink <43319904+BartJolink@users.noreply.github.com>
2018-09-18 13:46:43 +0200

    Add files via upload

--- Weektaak 3 inclusief zoekvenster.py
+++ Weektaak 3 inclusief zoekvenster.py
@@ -34,25 +34,33 @@ else:
     print("Dit is een mRNA-sequentie.")
     
 
-lengte = len(sequentie)
 aantal_GTCA = sequentie.count("G") + sequentie.count("T") + sequentie.count("C") + sequentie.count("A")
 aantal_GC = sequentie.count("G") + sequentie.count("C")
 aantal_DERK = sequentie.count("D") + sequentie.count("E") + sequentie.count("R") + sequentie.count("K")
-vergelijken = "n"
+lengte= len(sequentie)
 
-print("De lengte van de sequentie is "+str(lengte) +""".
+print("""
+De lengte van de sequentie is
+"""+str(lengte) +""".
 """)
 
 if str(header[17]) == "p":
-    print("Het aantal aminozuren D, E, R & K in de sequentie bedraagt: "+str(aantal_DERK)+""".
+    print("""Het aantal aminozuren D, E, R & K in de sequentie bedraagt:
+"""+str(aantal_DERK)+""".
+"""+"""
+Het percentage DERK ten opzichte van de gehele sequentie bedraagt:
+"""+str((int(aantal_DERK)/int(lengte))*100)+"""
 """)
+    
 elif str(header[18]) == "p":
-    print("Het aantal aminozuren D, E, R & K in de sequentie bedraagt: "+str(aantal_DERK)+""".
+    print("""Het aantal aminozuren D, E, R & K in de sequentie bedraagt:
+"""+str(aantal_DERK)+""".
+"""+"""
+Het percentage DERK ten opzichte van de gehele sequentie bedraagt:
+"""+str((int(aantal_DERK)/int(lengte))*100)+"""
 """)
-else:
-    print("")
-
-vergelijken = input("Wil je de lengte van de mRNA-sequentie vergelijken met de eiwit-sequentie? (y/n):")
+    
+vergelijken = input("Wil je de lengte van de sequentie vergelijken met een andere sequentie? (y/n):")
 
 if vergelijken == str("y"):
     print("Yes, kies nu het bestand waarmee je de huidige sequentie wilt vergelijken.")
@@ -61,3 +69,20 @@ else:
     sys.exit()
 
 bestand2 = open(askopenfilename(),"r")
+
+sequentie2= ""
+for line in bestand2:
+    if line.startswith(">"):
+        header2 = line
+    if not line.startswith(">"):
+        sequentie2 = sequentie2 + line
+
+sequentie2 = sequentie2.replace('\n','').replace('\r', '')
+
+lengte2= len(sequentie2)
+lengteverschil = int(lengte) - int(lengte2)
+
+print("""
+De lengte van de eerst gegeven sequentie is """+str(lengte)+""".
+De lengte van de tweede gegeven sequentie is """+str(lengte2)+""".
+Het verschil in lengte is """+str(lengteverschil)+".")
